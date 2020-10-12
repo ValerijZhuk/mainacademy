4e257a57f1aeb03481bf0ad8251e0abf2c9829d1
1 <1@DESKTOP-DGHD0JT>
2020-10-12 20:53:07 +0300

    Homework1

---
+++ Academy/src/by/academy/classwork/lesson1/Application.java
@@ -0,0 +1,16 @@
+package by.academy.classwork.lesson1;
+
+public class Application {
+	public static void main(String[] args) {
+	Cat Cat = new Cat();
+	Cat NameCat = new Cat("Вася");
+	NameCat.sleep();
+	NameCat.eat();
+	NameCat.walk();
+	Cat.grow();
+	Cat.grow();
+	Cat.grow();
+	System.out.println("Безымянному коту " + Cat.age + " года");
+	System.out.println(Cat.getMoney());
+	}
+}
---
+++ Academy/src/by/academy/classwork/lesson1/Cat.java
@@ -0,0 +1,33 @@
+package by.academy.classwork.lesson1;
+
+public class Cat {
+	
+		int age = 0;
+		String nickname;
+		double money = 1000;
+		public void grow(){
+			age = age + 1;
+			}
+		public void sleep(){
+			System.out.println("Кот спит");
+			}
+		public void eat(){
+			System.out.println("Кот ест");
+			}
+		public void walk(){
+			System.out.println("Кот гуляет");
+			}
+		public Cat(){
+			// nothing
+			}
+		public Cat(String nickname){
+			System.out.println(nickname);
+			}
+		public void setMoney (double money) {
+			this.money = money;
+		}
+		public double getMoney () {
+			return money;
+		}
+
+}
