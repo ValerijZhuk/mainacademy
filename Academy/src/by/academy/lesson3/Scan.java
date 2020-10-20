95b775e40c0e0d14c9a3c06d77cbe34c0caeb4f4
Murzik <Murzik@DESKTOP-613B03C>
2020-10-20 22:52:59 +0300

    Scanner

---
+++ Academy/src/by/academy/lesson3/Scan.java
@@ -0,0 +1,12 @@
+package by.academy.lesson3;
+import java.util.Scanner;
+public class Scan {
+
+	public static void main(String[] args) {
+		Scanner a = new Scanner(System.in);
+		System.out.println ("¬ведите 2 любых целых числа: ");
+		int b = a.nextInt();
+		int c = a.nextInt();
+		System.out.print(b + c);	
+	}
+}
