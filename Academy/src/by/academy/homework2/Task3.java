ee724af994d72c3d305b241ad257dbe84d98db25
Murzik <Murzik@DESKTOP-613B03C>
2020-11-03 20:41:09 +0300

    Task 3

--- Academy/src/by/academy/homework2/Task3.java
+++ Academy/src/by/academy/homework2/Task3.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class Task3 {
 
 	public static void main(String[] args) {
-		try (Scanner scan = new Scanner(System.in)) {
-			System.out.println("Введите число от 1 до 10:");
-			int a = scan.nextInt();
-			System.out.println("Таблица умножения для вашего числа от 1 до 10:");
-			for (int i = 1; i <= 10; i++) {
-							System.out.println(i * a);
-			}
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Введите число от 1 до 10:");
+		int a = scan.nextInt();
+		System.out.println("Таблица умножения для вашего числа от 1 до 10:");
+		for (int i = 1; i <= 10; i++) {
+						System.out.println(i * a);
 		}
 	}
 
