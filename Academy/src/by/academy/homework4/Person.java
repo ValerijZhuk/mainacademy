6c739b602b7e8333c84828d5d1710cab124d8fe3
Murzik <Murzik@DESKTOP-613B03C>
2020-11-22 22:18:45 +0300

    Deal homework

--- Academy/src/by/academy/homework4/Person.java
+++ Academy/src/by/academy/homework4/Person.java
@@ -1,18 +1,21 @@
 package by.academy.homework4;
 
+import java.util.Scanner;
+
 public class Person {
 	public static final Validator americanPhoneValidator = new AmericanPhoneValidator();
 	public static final Validator belarusianPhoneValidator = new BelarusianPhoneValidator();
 	public static final Validator emailValidator = new EmailValidator();
-//	public static Object emailValidator;
 	String name;
 	double cash;
-
+	DateOfBirth dateOfBirth = new DateOfBirth();
+		
 	public Person(String name, double cash) {
 		super();
 		this.name = name;
 		this.cash = cash;
 	}
+	
 
 	public Person() {
 		super();
@@ -33,4 +36,5 @@ public class Person {
 	public void setCash(double cash) {
 		this.cash = cash;
 	}
+	
 }
