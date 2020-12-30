b85b9eeda9e220ec3fcf31424e7b57981251e5da
Murzik <Murzik@DESKTOP-613B03C>
2020-12-30 11:36:54 +0300

    Homework

--- Academy/src/by/academy/homework8/Task1StreamLambda.java
+++ Academy/src/by/academy/homework8/Task1StreamLambda.java
@@ -9,12 +9,13 @@ public class Task1StreamLambda {
 
 	public static void main(String[] args) {
 		System.out.println();
+		final double Pi = 3.1415926536;
 
-		List<Long> ran = new Random().longs(100, 1, 100).map(x -> x * 3 - 20).filter(x -> x < 100).sorted().skip(3)
+		List<Long> ran = new Random().longs(100, 1, 100).map(x -> (long)(x * Pi - 20)).filter(x -> x < 100).sorted().skip(3)
 				.distinct().boxed().collect(Collectors.toList());
 		
 
-		Map<Long, String> m = ran.stream().collect(Collectors.toMap(x -> x, y -> "Number: " + y));
+		Map<Long, String> m = ran.stream().collect(Collectors.toMap(x -> x, y -> " Number: " + y));
 		System.out.println(m);
 
 	}
